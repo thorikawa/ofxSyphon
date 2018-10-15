83cca453ba6d6359923a5871bfdbf9a68ef65d99
Takahiro Horikawa <horikawa.takahiro@gmail.com>
2018-10-15 18:57:31 +0900

    Move __APPLE__ guard outside of import.

--- libs/Syphon/src/SyphonNameboundClient.m
+++ libs/Syphon/src/SyphonNameboundClient.m
@@ -26,10 +26,10 @@
     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
- 
-#import "SyphonNameboundClient.h"
 
 #ifdef __APPLE__
+
+#import "SyphonNameboundClient.h"
 
 @interface SyphonNameboundClient (Private)
 - (void)setClientFromSearchHavingLock:(BOOL)isLocked;
